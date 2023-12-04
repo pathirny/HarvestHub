ebe2a98d9612bb905664c1c868aa30ce59e09cf3
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-12-04 16:12:11 +0000

    grid

--- harvesthub/app/calendar/page.tsx
+++ harvesthub/app/calendar/page.tsx
@@ -1,7 +1,18 @@
-
-
-
 export default function Calendar(){
 
-    return(<>Hello</>)
+    return(
+    <div id="calendargrid">
+        <div className="month" id="january"></div>
+        <div className="month" id="february"></div>
+        <div className="month" id="march"></div>
+        <div className="month" id="april"></div>
+        <div className="month" id="may"></div>
+        <div className="month" id="june"></div>
+        <div className="month" id="july"></div>
+        <div className="month" id="august"></div>
+        <div className="month" id="september"></div>
+        <div className="month" id="october"></div>
+        <div className="month" id="november"></div>
+        <div className="month" id="december"></div>
+    </div>)
 }
