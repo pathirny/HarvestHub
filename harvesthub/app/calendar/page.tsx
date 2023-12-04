781784d78f47ce01e33cac0d423c096b085a7d7e
Olivia Johnson <olivia.1996@outlook.com>
2023-12-04 17:11:27 +0000

    calendar backgrounds

--- harvesthub/app/calendar/page.tsx
+++ harvesthub/app/calendar/page.tsx
@@ -1,22 +1,23 @@
-import Jan from "./assets/Jan.png"
-import Image from "next/image"
+import Header from "@/components/Header"
+
 export default function Calendar(){
 
     return(
         <div id="calendar-background">
+            <Header />
         <div id="calendargrid">
-            <div className="month" id="january"><p>January</p></div>
-            <div className="month" id="february"><p>January</p></div>
-            <div className="month" id="march"><p>January</p></div>
-            <div className="month" id="april"><p>January</p></div>
-            <div className="month" id="may"><p>January</p></div>
-            <div className="month" id="june"><p>January</p></div>
-            <div className="month" id="july"><p>January</p></div>
-            <div className="month" id="august"><p>January</p></div>
-            <div className="month" id="september"><p>January</p></div>
-            <div className="month" id="october"><p>January</p></div>
-            <div className="month" id="november"><p>January</p></div>
-            <div className="month" id="december"><p>January</p></div>
+            <div className="month br-wht" id="january"><p>January</p></div>
+            <div className="month wht-br" id="february"><p>Febraury</p></div>
+            <div className="month wht-br" id="march"><p>March</p></div>
+            <div className="month br-wht" id="april"><p>April</p></div>
+            <div className="month br-wht" id="may"><p>May</p></div>
+            <div className="month wht-br" id="june"><p>June</p></div>
+            <div className="month wht-br" id="july"><p>July</p></div>
+            <div className="month br-wht" id="august"><p>August</p></div>
+            <div className="month br-wht" id="september"><p>September</p></div>
+            <div className="month wht-br" id="october"><p>January</p></div>
+            <div className="month wht-br" id="november"><p>January</p></div>
+            <div className="month br-wht" id="december"><p>January</p></div>
         </div>
     </div>)
 }
