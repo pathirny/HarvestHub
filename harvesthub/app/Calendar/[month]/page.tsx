0cdab6067525dc554261286cdbed5a9a6357d8dc
Olivia Johnson <olivia.1996@outlook.com>
2023-12-05 09:31:49 +0000

    type for months params

--- harvesthub/app/Calendar/[month]/page.tsx
+++ harvesthub/app/Calendar/[month]/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header"
 import Link from "next/link"
 import BackButton from "@/components/BackButton"
 
-export default function Month({ params }){
+export default function Month({ params } : any){
 
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septmeber', 'October', 'November', 'December']
    const nxt = +params.month === 11 ? 0 : +params.month + 1 
@@ -10,7 +10,7 @@ export default function Month({ params }){
 
 return(<>
 <div id="calendar-pg-container" >
-<Header />
+<Header title="Month" />
 <div id="month-nav-container">
     <Link href={`./${prev}`}><div id="prv-mnth-bttn"></div></Link>
     <div className="month br-wht header-month" id="january"><p>{months[params.month]}</p></div>
