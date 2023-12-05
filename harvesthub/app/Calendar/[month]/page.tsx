e289bfec8dd8821e36867669f2e0eb161e82a08d
Olivia Johnson <olivia.1996@outlook.com>
2023-12-05 14:34:38 +0000

    list styling

--- harvesthub/app/Calendar/[month]/page.tsx
+++ harvesthub/app/Calendar/[month]/page.tsx
@@ -1,13 +1,24 @@
+"use client"
 import Header from "@/components/Header"
 import Link from "next/link"
 import BackButton from "@/components/BackButton"
+import { useEffect, useState } from "react"
+
 
 
 export default function Month({ params } : any){
 
+    const [plantList, setPlantList] = useState([{name: "carrot"}])
+    const [harvestList, setHarvestList] = useState([{name: "carrot"}])
+
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septmeber', 'October', 'November', 'December']
    const nxt = +params.month === 11 ? 0 : +params.month + 1 
    const prev = +params.month === 0 ? 11 : +params.month - 1 
+
+   useEffect(()=> {
+        setPlantList([{name: "carrot"}, {name: "sprouts"}, {name: "apple"}])
+        setHarvestList([{name: "pepper"}, {name: "pear"}, {name: "onion"}, {name: "sprout"}])
+   }, [])
 
 return(<>
 <div id="calendar-pg-container" >
@@ -24,8 +35,12 @@ return(<>
         <div>Plant</div>
         <div>Harvest</div>
     </div>
-    <div id="plant-list">Plant</div>
-    <div id="harvest-list">Harvest</div>
+    <div id="plant-list">
+        {plantList.map((a) => {return (<><h1 key={a.name} className="plantListItem" >{a.name}</h1></>)})}
+    </div>
+    <div id="harvest-list">
+        {harvestList.map((a) => {return (<><h1 key={a.name} className="harvestListItem">{a.name}</h1></>)})}
+    </div>
  </div>
 <Link href='/Calendar'><BackButton/></Link>
 </div>
