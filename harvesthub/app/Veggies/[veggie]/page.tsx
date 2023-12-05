cd29d2a0310d4a42c33e8a3489c899e70c7d91b8
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-12-05 16:18:36 +0000

    veggie page

--- harvesthub/app/Veggies/[veggie]/page.tsx
+++ harvesthub/app/Veggies/[veggie]/page.tsx
@@ -1,10 +1,24 @@
-
+import Header from "@/components/Header";
 
 export default function Veggies(){
 
 
     return(<>
-    
+    <Header></Header>
+    <div id="veggie-container">
+        <div id="veggie-general-info">
+
+        </div>
+
+        <div id="veggie-grid">
+        <div className="cream">Difficulty:</div>
+        <div className="cream">Time:</div>
+        <div className="cream">Plant:</div>
+        <div className="cream">Harvest:</div>
+        </div>
+
+
+    </div>
     
     </>)
 }
