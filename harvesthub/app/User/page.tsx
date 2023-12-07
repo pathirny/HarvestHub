73d9609e8f7fe0e3b65fd359823126d89a842666
jamesthecooke <jamesjcooke03@gmail.com>
2023-12-07 11:58:49 +0000

    added the start of user page - stled kinda

--- harvesthub/app/User/page.tsx
+++ harvesthub/app/User/page.tsx
@@ -1,16 +1,29 @@
-"use client"
-import { SettingsIcon, AddIcon } from "@chakra-ui/icons"
-export default function UserPage(){
-
-return(<>
-<div id="userPage-container">
-    <div id="icon-container">
-        <SettingsIcon />
-        <AddIcon />
-    </div>
-    <div id="user-img"><img></img></div>
-</div>
-</>)
-
-
+"use client";
+import Header from "@/components/Header";
+import { SettingsIcon, AddIcon } from "@chakra-ui/icons";
+export default function UserPage() {
+  return (
+    <>
+      <Header title="User Page" />
+      <div id="userPage-container">
+        <div id="icon-container">
+          <div className="IconBox">
+            <SettingsIcon boxSize="5vw" />
+          </div>
+          <div className="IconBox">
+            <AddIcon boxSize="5vw" />
+          </div>
+        </div>
+        <div id="user-img">
+          <img></img>
+        </div>
+        <p style={{alignSelf:'center'}}>Hello im a user</p>
+        <div id="Underline"></div>
+        <div className="UserBio">
+          <h2 style={{textAlign:'center'}} >Bio</h2>
+          <p style={{textAlign:'center'}} >This is my bio </p>
+        </div>
+      </div>
+    </>
+  );
 }
