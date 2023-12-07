636024e5830f1bacc8c71be6101a54ce3f54a113
Olivia Johnson <olivia.1996@outlook.com>
2023-12-07 12:22:31 +0000

    user img

--- harvesthub/app/User/page.tsx
+++ harvesthub/app/User/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Header from "@/components/Header";
+import BackButton from "@/components/BackButton";
 import { SettingsIcon, AddIcon } from "@chakra-ui/icons";
+import Link from "next/link";
+import { CldImage } from 'next-cloudinary';
 export default function UserPage() {
   return (
     <>
@@ -8,22 +11,34 @@ export default function UserPage() {
       <div id="userPage-container">
         <div id="icon-container">
           <div className="IconBox">
-            <SettingsIcon boxSize="5vw" />
+            <SettingsIcon boxSize="7vw" />
           </div>
           <div className="IconBox">
-            <AddIcon boxSize="5vw" />
+            <AddIcon boxSize="7vw" />
           </div>
         </div>
-        <div id="user-img">
-          <img></img>
+        <div id="user-img" >
+        <CldImage  id="user-img" className="tip-img"
+    alt="tip-img"
+        width="500"
+        height="500"
+        style={{width: "70vw", height: "auto", borderRadius: "50%", margin: "auto"}}
+        src={`/HarvestHub/Pumpkin`}
+        // src="/HarvestHub/Pumpkin"
+        />
         </div>
-        <p style={{alignSelf:'center'}}>Hello im a user</p>
+        <div id="user_name">
+        <p style={{alignSelf:'center'}}>John Doe</p>
+        </div>
         <div id="Underline"></div>
-        <div className="UserBio">
-          <h2 style={{textAlign:'center'}} >Bio</h2>
-          <p style={{textAlign:'center'}} >This is my bio </p>
+          <h2 id="bio-title" style={{textAlign:'center'}} >Bio</h2>
+        <div id="UserBio">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
+      <Link href="/">
+      <BackButton />
+      </Link>
     </>
   );
 }
