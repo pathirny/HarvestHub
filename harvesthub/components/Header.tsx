66753dadbc6c5c86fba21a26ca07deca4dc86549
Olivia Johnson <olivia.1996@outlook.com>
2023-12-09 19:13:55 +0000

    updates

--- harvesthub/components/Header.tsx
+++ harvesthub/components/Header.tsx
@@ -3,21 +3,19 @@ import Link from "next/link"
 export default function Header({ title } : any) {
   return (
     <>
-    <div id="nav-body">
+    <header id="nav-body">
     <div id="nav-container">
     <Link href="/"><div id="home-button">
             <h3>Home</h3> 
        </div></Link>
-       <div id="page-title">
-            <h3>{title}</h3>
-       </div>
+            <h1 id="page-title">{title}</h1>
        <Link href="/User">
        <div id="user-button">
             <h3>User</h3>
        </div>
        </Link>
     </div>
-    </div>
+    </header>
     </>
   )
 }
