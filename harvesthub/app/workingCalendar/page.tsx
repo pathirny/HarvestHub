da3849218f0b427fb8a1f60ef15b03f842f30e22
Olivia Johnson <olivia.1996@outlook.com>
2023-12-11 13:59:28 +0000

    adding events

--- harvesthub/app/workingCalendar/page.tsx
+++ harvesthub/app/workingCalendar/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-
 import FullCalendar from '../../components/calendar.jsx';
 
 export default function Home() {
   return (<>
-  
+
     <div id="calendar-container">
       <FullCalendar initialView='dayGridMonth' />
       {/* <FullCalendar initialView='timeGridWeek' /> */}
--- harvesthub/components/calendar.jsx
+++ harvesthub/components/calendar.jsx
@@ -6,6 +6,9 @@ import { useState } from 'react';
 
 export default function FullCalendar(props) {
 
+  const [input, setinput] = useState(false)
+  const [date, setDate] = useState(null)
+
   const [event, setEvent] = useState([{ // this object will be "parsed" into an Event Object
     title: 'Christmas', 
     start: '2023-12-25', 
@@ -15,42 +18,50 @@ export default function FullCalendar(props) {
     textColor: '#47594e',
     borderColor: '#f3ebe4',
     eventOverlap: true
-  }, {
-    title: 'not christmas', 
-    start: '2023-12-12', 
-    allDay: true, 
-    display: 'block',
-    backgroundColor: '#f3ebe4',
-    textColor: '#47594e',
-    borderColor: '#f3ebe4',
-    eventOverlap: true
-  }, {
-    title: 'still not christmas', 
-    start: '2023-12-24', 
-    allDay: true, 
-    display: 'block',
-    backgroundColor: '#f3ebe4',
-    textColor: '#47594e',
-    borderColor: '#f3ebe4',
-    eventOverlap: true
   }])
 
   function handleDateClick(e){// bind with an arrow function
-    console.log(e.date)
-   setEvent((curr)=>{return [...curr, {
-    title: 'also not christmas', 
-    start: e.date, 
-    allDay: true, 
-    display: 'block',
-    backgroundColor: '#f3ebe4',
-    textColor: '#47594e',
-    borderColor: '#f3ebe4',
-    eventOverlap: true
-  }]})
+    setinput(true)
+    setDate(e.date)
   }
 
+  function addEvent(data){
+    console.log(date)
+
+
+    let newDate = (date.getDate() + 2)
+
+    setEvent((curr)=>{return [...curr, {
+      title: data.target[0].value, 
+      start: date,
+      allDay: true, 
+      display: 'block',
+      backgroundColor: '#f3ebe4',
+      textColor: '#47594e',
+      borderColor: '#f3ebe4',
+      eventOverlap: true
+    }, {
+      title: data.target[0].value + "second", 
+      start: newDate,
+      allDay: true, 
+      display: 'block',
+      backgroundColor: '#f3ebe4',
+      textColor: '#47594e',
+      borderColor: '#f3ebe4',
+      eventOverlap: true
+    }, ]})
+
+    setinput(false)
+  }
   
 
-  return <Calendar plugins={[dayGrid, timeGrid, interactionPlugin]} selectable={true} dateClick={(e)=>{handleDateClick(e)}}   events={event} eventClick={(e)=>{addCheck}}{...props}  />;
+  return (<>
+  { input ? 
+  <form onSubmit={addEvent}>
+    <label htmlFor='vegTypev'>what are you growing</label>
+    <input type="text" name="vegType"/>
+    <button type='submit'>Submit</button>
+  </form> : <></>}
+  <Calendar plugins={[dayGrid, timeGrid, interactionPlugin]} selectable={true} dateClick={(e)=>{handleDateClick(e)}}   events={event} eventClick={(e)=>{addCheck}} {...props}  /></>);
 
 }
