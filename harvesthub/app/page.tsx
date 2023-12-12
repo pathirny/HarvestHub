d3df48ebf95823338003c11a3632bc368370c095
Olivia Johnson <olivia.1996@outlook.com>
2023-12-12 09:48:09 +0000

    link to calendart

--- harvesthub/app/page.tsx
+++ harvesthub/app/page.tsx
@@ -120,7 +120,7 @@ export default function Index() {
           </div>
         </Link>
         <div className="link-container">
-          <Link href="#">
+          <Link href="/workingCalendar">
             <div className="link-container" style={{ padding: "0" }}>
               <div
                 style={{
@@ -133,7 +133,7 @@ export default function Index() {
                 }}
               ></div>
               <h2 style={{ position: "absolute", fontSize: "2rem" }}>
-                Stretch goal 1
+                Personal Calendar
               </h2>
             </div>
           </Link>
