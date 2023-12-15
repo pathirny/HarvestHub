302222e04534c9877380b656adcdd68e4566322a
Patryk Hirny <pathirny@googlemail.com>
2023-12-15 09:24:20 +0000

    fixed header pictures

--- harvesthub/app/login/page.tsx
+++ harvesthub/app/login/page.tsx
@@ -42,7 +42,7 @@ export default function Login({
       email,
       password,
       options: {
-        emailRedirectTo: "www.google.com",
+        emailRedirectTo: `${origin}/auth/callback`,
       },
     });
 
